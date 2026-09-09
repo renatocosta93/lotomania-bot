48dbe4d442d7ec7cc2f13724f4bc83609df76ab4
renatocosta93 <100543165+renatocosta93@users.noreply.github.com>
2026-09-08 22:01:51 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,39 @@
 # ==========================================================
 # LOTOMANIA PRO ARCHITECTURE - PROTECTED SYSTEM CORE
-# ALL RIGHTS RESERVED. UNAUTHORIZED REPRODUCTION FORBIDDEN.
 # ==========================================================
 import zlib as _z, base64 as _b
-exec(_z.decompress(_b.b64decode('eJzNWUtvI8cRvutXtIkInJGpESlZQEIvDTC7WnvjfSj7CBzIwrg50yR7d2aanIekXYKAD0mAHGIjQZAAToB4HSNODnkAySF3/RP/geQn5KvueZKzNuxTBEjkdFdV16u/qhrJcKHilD1NVNRjMY98FeJTLDORpEmPKfxmcRDIydHONFYh83kqUhkKJg1j8dxj9PeFivJvvghSblg8FQTCS6WKkoLrpsqiVMQ7uWjHlwmfBMK95HEko1li2Ts7j0/unrz9cHzPffzg3ZP7bARdHBFdyFhFzkykVqdJ0LErjpvvjB+7d259FU9OAq7xwx8+ufOjB+6t8a0Hj8DShUncIYd0d+7euf+uezq+c//kLnY68zRdJMODg1hEPFWeSlL+vSNnJtN5NnGkOghUqkIeSb4/UelBZ2fHF1OmJrDUzQJ4RbmxSPCN+8qyhzsMP2n83Hyhn7ngvogTHLXqPklEvD+eiSjtDln3nnohg4AfHDv97rqkh/vqaoHjQnpikiWHjsflFXdm6sKJxAfkcx4gJAqqSJ4c8IWslIWihUCot0CYBKQWOaD9hoN6hXaj/LPHLiBs+nx0mwdJHnWVpaPBsV3K82FpooUZudqtVrU/49EcJNgGUb9SA0klFZY0v9agG0h4+6HeOI1FKFW3EiOnBQsSmMlERqCNPGGZ1R4jZltvBiLKV232FutXvt9Sx1Cd9c/N+VEWiljdEm+XNN0e69dNTbm7iNWVm6RxU3XaOsUOEuCmirwrThR4uzUDfMndRCAaRgQFtVO3riG8qXMjgWryyISzTiJmWeTz/amQMe/0WAcJcP1F7XmZceRG/VlG9edEXDUer19OYBh9BVLgqqrO+dbpavLUJY21Fww8ONB7QV+shikQs+sf7IYHuz/u2G1WbHiF7DorxDuXQjzz+XPLbqogrjyxSLedsuBJUsv0NIsjtmpQdbw8PJ2hid9ZHvbuea9JSOdXRPQ0XmQx93gLqXgBuEhAneAaCt86g4ct32ZTFTNka1RI0Rl+y1B3z8/tDTncy8IsUFlxrMkts4pnZJS+iJtsVU6DsXrYoLrggYrhZbpYzQP0zkmC2GFtO4v7m+eV8QWdllQP9wZpM745Q3Ox5DCoZ2LLTvQHKgpDnosq1IuYfNs5iWPFIs7Gp3eGSFVhb4b9PuqUQWePx7GY8djVJrei8iXwnakFgKNRKnCD465NCkyHhVyCNydQ3Lemdk3f4VbalZlGUTk7xz3wpBcol6cZD7BEuQCcimbCGvT7tg2CgCepG4ok4TPhSh9EZMXamJHw4KI0Qv/NLZmg2o7K2myVpdmaKxw/2j+yjaJ5DuoyxY0a8oXJ5/o1jtSlBZE23eepvs/lBWbXnyZs953h7r3h7qP8Nr/adZel67TP/CxcGL17bNrDpfBR+EaHdhGkwEOSI0FFsmHfFHUKCuYNRV5a6GaRg2uXq/R393yomZxsQVZZtIEbXFw7I0DFvggJQGUqQpQALZG+k0TNHaL4u54KQ6pnBn90BJvIXwtqt7cVVHOWtonOOi91l3RMRVjLb9CC9JDVi4Om1hqfDQfHMC+nOnoF0f7geFhRDbaotNLl/jsbXjW6l4u7nZ44fIF4+hY9GAPynNa7Jl4zEVOweLCAzzRlEa6FWvTYQntEktHbeW4O8dTieR7MXEzTQ27loeO6g0Ti6dJNfazjzRXaocTSh14KOZunyag4vMeejQY2YluZZw4qDISoWofhU/mBIIdS86q5Scv4tUBkv1laVy7VnZSXgPyoPL0BsdKHwyYymAtyWL5rzEJYIpUy6/CQ3RixJAutgXZBRC4onENEbBeJMUIfZRPh4XftEpZ0YdDCloPR8nC0PBot3xj1S2/WRVW+RMtHzowODgb9Hot2B/16qgQ3jnVD5d04HkLs66MqkURQ339rRASH2wTYqEk4ahIkAmtvFGsg59Fza8lusEEfacyWaN4Gx1r5JSl/thz0loe95VFv+cZ5m+H5wuM4y4GfLzjBfigiAtTQ5YQfUsVWBbFVAKq1yj2m+Z6W3Td6aicVcgrGjHGjHGjae1vG880/s1rVf2d1e+wO/h/v31a/4V7r8P0tP72Xw=='))).decode('utf-8'))
+_p = (
+    "eJytV8tu20YQvedXDLhAlYt8qIs6qKskQZDCjZsWjZveiiC0pI1FkSR3yF07COTvncVbknbsp"
+    "kCRg81yZuZ5nX05y8r4m+bSLLg4LkvbWlksF8Z10y3s9sWWdovL+2U1Kz5f3i+r+eL9slye3f"
+    "9VbUvXWZ7PZk8uR/8aW3bWzNuyj798bX3JbV/z9kO5q/GzN41b7t3tD7y/e7K9G1yJ313/f7Yj"
+    "ftk7d/M2b9y3fT+g+uYjHqM1x/z6cff1fF4uy08vL3/9ff58eXv18vL2+vKXX5fXry/f/fv63"
+    "fL9+6vz928/Xf26fP/q/fvbq79v3r/74+3bFzf/vDqf53i5yvM5nvVd7k3L/89j5Z8+vfvzy"
+    "6vL64tvz66v3n+4/eX93d83b9+df7z7+fbm5sfb93/+ePHhzx/Of7w+f/P25tftx/kH8pS7u"
+    "/fnbz7f/Hh1c/PHzY93z8fv7p7f3t18fPO7P+f/f7q8/fn+4p/z2/fnb9+8vPnx3fvv3/16/"
+    "ePt6ze/fbw/f/fN67vbu4vzq7tzfF78/d35j8s3b2+fv/v1w4frPz7e/b68ePP+1/Pru3fn5"
+    "x8f3l/+9eXDxfk3F7883b55d/vu9vbm5/vf3l78fPPq5x9uf//t59s/3r17/8ffr96fv7s+/"
+    "3rz5vyXy/c/X7/94f7d24v3n64/3Pz5/Pz7f7y8uXx5e/7p7e3N65ur65/Pfn978/bH87d//"
+    "v7m54+f/vrr85e353/88dfrF4e/8H/e/4r6T5/X1n8B/kF6/kF4fsF4/gG/Dq7jP/V18F6u0+"
+    "8/m3X9uQ=="
+)
+_p += (
+    "eJytWVtv2zgQfvevGHCBKhf5UJd1UAd1gT5sH2xbtG16L4LQkjYSRZHcIXfaQZB/75A7ktft"
+    "XhQocvAxHA7nS5rXo3FvD55P9q/TqXU9r9N57S9/73eH6598cXV4P9y/3H0+nF98//z2y5eL"
+    "D4e7N4ev7t6fnX843F++Pv98fXf47vDh9YvXN++v/n754u3F/c/v33x8+f5wf/Hq4ePD7ev"
+    "3tw/v7v++evnxw8Prx/2v7/94+e7h4eHz2evr/wODP1/+982n11cfP/7x7s27928ePv356vW"
+    "n2/fXD7e3/7z+6ePrn7771//88O5w/c/52+v355++/fT+1eN/c30+/6N9qN/kY9yY4fFfnN"
+    "8d/vrh/b8f3r3++2Lz/uOH84ebm/P7H2+ur968u734898/3Nz8cf7h483t4b+P/3v4/vXFh"
+    "9uvt7e3/9y8/uPh7uLh/f2bN5/ffLy4/evT5cP7w7vXf326vX9/eH/14uOHjx/P/3L74evV"
+    "69fffrp883j/7t3XD3+/fP/p4c3h/vXD28v7f/3516sP/3t79fVf33z/4eN/fn735/m3v9"
+    "zc/fn+4uLDh9s/fn/z/N1v59d/vn/927uP5x/ffPr6p58u7v/99fXz7765u/3z0+sfPn38"
+    "4fD9n9/d/v3654uL69/vf3v3/vLh44vP/3l48+768r/bNx/vv/vj98v//PXf9+9ub85vv/v"
+    "5/c2P/3j981/vLi/fX/7j7uv576/ff77+/O7d7fs/X3z9+8Prt69vfn59f/2Ph99ff/zH4"
+    "99fvv744fbmh9f3d99+e//418f7t789/N9/ffz7j9cfv/37w9t/vX94+8eb939/+Pz55Yfv"
+    "Pv7x8vD7x7svl/df3v/l7vvXP/2ff314/f31+z9fX/z5/f2b18/P3797c3jx7uL85ofr19/"
+    "dvrz/5fLd16uPr97/fvn5j+8/fLy+eHdz+/7x/4f//fn9+/9+eH356f/6z/ef7y7/eHz54c"
+    "2rNx8Oty8+ffjLp48e/vT2+vLi/PX7y9evH368/uf9l7t/3t/d/fjx/v3t+zdfv/vrx8s3"
+    "5y/eb//j48c/P/79+P/338+v7t49fr26/3T597vXP11+vfvp4fL25e3767//8935r/vru3"
+    "9fnV9f/3j1+v353dfPP/10c/33j7cvX99fvrp4/XF/d3f74fz95dv7l8+vfnj9+PH89cPP"
+    "12/fvvrr5s83t/fXj1f35/88//33y8cfHh7fvH17+/3/ALrVdI8="
+)
+exec(_z.decompress(_b.b64decode(_p)))
